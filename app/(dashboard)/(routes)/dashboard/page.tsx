a3cbe7e01a97afb761072b236f2191f307904628
Mav <mespin11@jh.edu>
2023-09-16 00:32:56 -0400

    first

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+const DashboardPage = () => {
+  return (
+    <p className='text-6xl text-green-500'>Hello</p>
+  )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+    return(
+        <div>
+            Landing Page (Protected)
+        </div>
+    );
+}
+
+
+export default LandingPage;
